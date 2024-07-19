5b99faab89d83a786767a06acdbf3cf417e8f713
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-07-19 18:07:48 -0500

    feat: Creación de Pruebas Unitarias Pt 2 udistrital/argo_documentacion/issues/71

--- src/cdp/cdp.service.spec.ts
+++ src/cdp/cdp.service.spec.ts
@@ -34,6 +34,43 @@ describe('CdpService', () => {
   });
 
   describe('obtenerCDP', () => {
+    it('deberia retornar una lista de CDP', async () => {
+      const mockCdp: CdpDto = {
+        vigencia: '2023',
+        descripcion: 'Test CDP',
+        rubro_interno: '123',
+        estado: 'Activo',
+        justificacion: 'Test justification',
+        id_sol_cdp: '456',
+        nombre_dependencia: 'Test Dept',
+        fecha_registro: '2023-01-01',
+        observaciones: 'Test observations',
+        numero_disponibilidad: '789',
+        num_sol_adq: '101112',
+        valor_contratacion: '1000000',
+        estadocdp: 'Aprobado',
+      };
+
+      mockedAxios.get.mockResolvedValue({
+        data: {
+          informacion_cdp: {
+            cdp: [mockCdp],
+          },
+        },
+      });
+
+      const result = await service.obtenerCDP('2023', '789', '1');
+
+      expect(result).toEqual({
+        Success: true,
+        Status: HttpStatus.OK,
+        Message: 'CDP Encontrado',
+        Data: [mockCdp],
+      });
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        'http://mock-endpoint/2023/789/1',
+      );
+    });
 
     it('deberia retornar CDP No encontrado', async () => {
       mockedAxios.get.mockResolvedValue({
@@ -64,5 +101,26 @@ describe('CdpService', () => {
       });
     });
 
+    it('deberia retornar un eror de axios', async () => {
+      const errorMessage = 'Bad Request';
+      const errorResponse = {
+        response: {
+          status: HttpStatus.BAD_REQUEST,
+        },
+        message: errorMessage,
+      };
+      mockedAxios.get.mockRejectedValue({
+        ...new Error(errorMessage),
+        ...errorResponse,
+      });
+
+      const result = await service.obtenerCDP('2023', '789', '1');
+
+      expect(result).toEqual({
+        Success: false,
+        Status: HttpStatus.BAD_REQUEST,
+        Message: errorMessage,
+      });
+    });
   });
 });
