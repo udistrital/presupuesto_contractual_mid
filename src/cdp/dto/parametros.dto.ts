75813b563312cd0eadfabce117f7b126265e1282
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-08-01 01:19:12 -0500

    feat: Actualización del service udistrital/argo_documentacion/issues/77

--- src/cdp/dto/parametros.dto.ts
+++ src/cdp/dto/parametros.dto.ts
@@ -1,11 +1,19 @@
 import { IsNotEmpty } from 'class-validator';
 
-export class ParametrosDto {
+export class ParametrosDetalleDto {
   @IsNotEmpty()
   vigencia: string;
 
   @IsNotEmpty()
   numeroDisponibilidad: string;
+
+  @IsNotEmpty()
+  unidadEjecutora: string;
+}
+
+export class ParametrosListaDto {
+  @IsNotEmpty()
+  vigencia: string;
 
   @IsNotEmpty()
   unidadEjecutora: string;
